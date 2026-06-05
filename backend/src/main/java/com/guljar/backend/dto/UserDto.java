1300ac1c5b858089ab564d95e11ba199554dd68d
Guljar Hussain <guljarhussain7865@gmail.com>
2026-06-06 01:08:40 +0530

    feat: add safe UserDto representation profile

---
+++ backend/src/main/java/com/guljar/backend/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.guljar.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private boolean isActive;
+    private List<String> roles;
+}
