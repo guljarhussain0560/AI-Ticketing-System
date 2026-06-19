8e004fc021105cbe75518875acc5a88aacaf0ab7
Guljar Hussain <guljarhussain7865@gmail.com>
2026-06-20 01:47:09 +0530

    feat: create UserService interface

---
+++ backend/src/main/java/com/guljar/backend/service/UserService.java
@@ -0,0 +1,14 @@
+package com.guljar.backend.service;
+
+import com.guljar.backend.dto.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserDto> getAllUsers();
+    UserDto getUserById(Long id);
+    UserDto getUserByUsername(String username);
+    UserDto updateUser(Long id, UserDto userDto);
+    void deleteUser(Long id);
+    UserDto toggleUserActiveStatus(Long id);
+}
